eef405b241b149631db21cf8a4197bf2e61da2ef
Robert Chen <robertchen@live.com>
2019-04-02 22:20:28 -0700

    Basic login system working

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
 var app = express();
+
+const session = require('express-session');
+// Sessions
+app.use(session({
+  secret: process.env.SECRET || 'cas-project ihs',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +27,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const requireAuth = (req, res, next) => {
+  if(req.session.username){
+    next();
+  }else{
+    req.session.error = "Login required";
+    res.redirect("/login");
+  }
+}
+
+// https://stackoverflow.com/questions/5251520/how-do-i-escape-some-html-in-javascript/5251551
+function escapeHTML(s) { 
+    return s.replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+}
+
+app.use(function(req, res, next){
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + escapeHTML(err) + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + escapeHTML(msg) + '</p>';
+  next();
+});
+
+app.use('/login', loginRouter);
+app.get("/logout", (req, res) => {
+  req.session.destroy(() => res.redirect("/"));
+});
+app.use('/', requireAuth, indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
