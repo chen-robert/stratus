e337767711ce480b9c53ee0ef2a8f34aaa6e6ec2
Robert Chen <robertchen@live.com>
2019-04-11 13:38:01 -0700

    Added class page + Improved index layout

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: req.session.username });
 });
 
+router.get("/class", (req, res) => {
+  res.render("class", {title: "Math HL 1"});
+});
+
 module.exports = router;
