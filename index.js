d9eb6be33a4b0a151466eff61504994009b4024a
Robert <robertchen@live.com>
2019-11-07 17:27:11 +0000

    Fixed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 const cookieSession = require("cookie-session");
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
